66defb8560f417522c85e03f0b214ee47d66caab
M1tDan <116488378+M1tDan@users.noreply.github.com>
2024-05-03 08:38:10 +0300

    NewTimer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public GameObject NextDay;
 
-    public float timerSpeed = 0.5f;
+    private float timerSpeed = 0.02f;
     private int minutes = 0;
     private int hours = 6;
-    private float currentArrow = 136f;
     public int pause = 1;
     public TMP_Text _TimerText, currentDayText;
-    [SerializeField] private int delta = 0, currentDay = 1;
-    public GameObject ArrowPivot;
+    [SerializeField] private int currentDay = 1;
+
+    private float hoursForTimer = 600;
+    public Slider TimerSlider;
 
     private const string DayNumber = "DayNumber";
 
@@ -22,31 +24,33 @@ public class Timer : MonoBehaviour
     {
         while (pause == 1)
         {
-            if (minutes == 59)
-            {
-                hours = hours + 1;
-                minutes = -1;
-            }
-            minutes += delta;
-            _TimerText.text = hours.ToString("D2") + ":" + minutes.ToString("D2");
-            currentArrow = currentArrow - 0.3f;
-            ArrowPivot.transform.rotation = Quaternion.Euler(0, 0, currentArrow);
-            if (hours == 21)
+            if (hours == 6 + hoursForTimer/60)
             {
                 pause = 0;
                 NextDay.SetActive(true);
                 PlayerPrefs.SetInt(DayNumber, currentDay + 1);
                 PlayerPrefs.Save();
             }
+            if (minutes == 60)
+            {
+                hours = hours + 1;
+                minutes = 0;
+            }
+            if (minutes % 5 == 0)
+            {
+                _TimerText.text = hours.ToString("D2") + ":" + minutes.ToString("D2");
+            }
             yield return new WaitForSeconds(timerSpeed);
+            minutes += 1;
+            TimerSlider.value = ((hours * 60) + minutes) - 360;
         }
     }
 
     private void Start()
     {
-        //PlayerPrefs.DeleteAll(); //закоментировать если нужно обнулить дни
+        //PlayerPrefs.DeleteKey("DayNumber"); //закоментировать если не нужно обнулять дни
+        TimerSlider.maxValue = hoursForTimer;
         StartCoroutine(DayTimer());
-        ArrowPivot.transform.rotation = Quaternion.Euler(0, 0, 136);
         if(PlayerPrefs.GetInt("DayNumber") == 0)
             currentDay = 1;
         else
