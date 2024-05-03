a67c4ed9bbee97f2c5276de5799bcf98c8a3e847
M1tDan <116488378+M1tDan@users.noreply.github.com>
2024-05-03 16:12:44 +0300

    NewPlayerModel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,5 +18,52 @@ public class PlayerController : MonoBehaviour
         _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
 
         animator.SetFloat("HorizontalMove", _joystick.Horizontal);
+
+        if (Input.GetKey(KeyCode.A) & Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.velocity = new Vector3(-0.75f * _moveSpeed, _rigidbody.velocity.y, 0.75f * _moveSpeed);
+            animator.SetFloat("HorizontalMove", -1f);
+        }
+        else if (Input.GetKey(KeyCode.S) & Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.velocity = new Vector3(0 * _moveSpeed, _rigidbody.velocity.y, 0 * _moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D) & Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.velocity = new Vector3(0.75f * _moveSpeed, _rigidbody.velocity.y, 0.75f * _moveSpeed);
+            animator.SetFloat("HorizontalMove", 1f);
+        }
+        else if (Input.GetKey(KeyCode.A) & Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.velocity = new Vector3(0 * _moveSpeed, _rigidbody.velocity.y, 0 * _moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A) & Input.GetKey(KeyCode.S))
+        {
+            _rigidbody.velocity = new Vector3(-0.75f * _moveSpeed, _rigidbody.velocity.y, -0.75f * _moveSpeed);
+            animator.SetFloat("HorizontalMove", -1f);
+        }
+        else if (Input.GetKey(KeyCode.D) & Input.GetKey(KeyCode.S))
+        {
+            _rigidbody.velocity = new Vector3(0.75f * _moveSpeed, _rigidbody.velocity.y, -0.75f * _moveSpeed);
+            animator.SetFloat("HorizontalMove", 1f);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.velocity = new Vector3(0 * _moveSpeed, _rigidbody.velocity.y, 1f * _moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _rigidbody.velocity = new Vector3(-1f * _moveSpeed, _rigidbody.velocity.y, 0 * _moveSpeed);
+            animator.SetFloat("HorizontalMove", -1f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _rigidbody.velocity = new Vector3(0 * _moveSpeed, _rigidbody.velocity.y, -1f * _moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.velocity = new Vector3(1f * _moveSpeed, _rigidbody.velocity.y, 0 * _moveSpeed);
+            animator.SetFloat("HorizontalMove", 1f);
+        }
     }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,14 +8,14 @@ public class Timer : MonoBehaviour
 {
     public GameObject NextDay;
 
-    private float timerSpeed = 0.02f;
+    private float timerSpeed = 0.2f;
     private int minutes = 0;
     private int hours = 6;
     public int pause = 1;
     public TMP_Text _TimerText, currentDayText;
     [SerializeField] private int currentDay = 1;
 
-    private float hoursForTimer = 600;
+    private float hoursForTimer = 900;
     public Slider TimerSlider;
 
     private const string DayNumber = "DayNumber";
